809ca7a3f17a5b26beb3d0827a0500b442e181a3
uppermooninfinity <uppermooninfinity@gmail.com>
2026-05-27 22:48:19 +0530

    Update Youtube.py

--- Oneforall/platforms/Youtube.py
+++ Oneforall/platforms/Youtube.py
@@ -8,7 +8,7 @@ import httpx
 import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
-from youtubesearchpython.__future__ import VideosSearch
+from py_yt import VideosSearch
 
 
 from Oneforall.utils.database import is_on_off
